910f7d37c17a88128af2909b75ad0eaaa2b90c26
sujin <csujin2040@gmail.com>
2024-04-29 13:29:45 +0900

    [page] login, sign up 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_blog/page/appbar.dart';
 import 'package:flutter_application_blog/page/login.dart';
 
 void main() {
@@ -10,7 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: login(),
+      home: Scaffold(
+        body: login(),
+      ),
     );
   }
 }
